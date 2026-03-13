2b15cc0c5c1e7f16b0b9a699e3cf769377cc2674
Your Name <amoh04403@gmail.com>
2026-03-13 12:27:30 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello Welcome to the Classroom Backend!");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
